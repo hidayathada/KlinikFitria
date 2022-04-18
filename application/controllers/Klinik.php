50696c7921444eb403141a5e36d58a249cd3051a
hidayathada <hidayathada@gmail.com>
2022-04-18 11:35:03 +0700

    first commit

---
+++ application/controllers/Klinik.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Klinik extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index(){
+        $this->load->view("template/head.php");
+        $this->load->view("template/navbar.php");
+        $this->load->view("v_index.php");
+    }
+
+}
