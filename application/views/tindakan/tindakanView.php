ef00a2a9c296af437c11bf47ef1cffe359df8464
akramfd12 <umarofahrul@gmail.com>
2022-04-20 14:59:34 +0700

    Menambah Tittle TIndakan

--- application/views/tindakan/tindakanView.php
+++ application/views/tindakan/tindakanView.php
@@ -1,4 +1,5 @@
 	<!-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css"> -->
+	<title>Tindakan</title>
 
 	<body>
 		<div class="container mt-5">
